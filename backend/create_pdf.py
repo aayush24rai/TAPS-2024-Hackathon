da66f265a279de8b2b8f6d444cbd0b081410059d
kroy2022 <kevinroy2015@gmail.com>
2024-10-24 21:26:08 -0500

    email addition

--- backend/create_pdf.py
+++ backend/create_pdf.py
@@ -1,10 +1,12 @@
 from fpdf import FPDF
-import datetime
-import os 
+import datetime, os, smtplib, threading
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
 pdf = FPDF()
 
-def create_pdf(optimized_irrigation, plot_number, dollar_cost, other_variables=None):
+def create_pdf(email, optimized_irrigation, plot_number, dollar_cost, other_variables=None):
     pdf_directory = os.getcwd()
     pdf.add_page()
 
@@ -67,5 +69,56 @@ def create_pdf(optimized_irrigation, plot_number, dollar_cost, other_variables=N
     pdf.cell(0, 10, f"The cost of irrigation this week is ${dollar_cost} based on the gallon price in Colby, Kansas at $1.05")
 
     pdf.output(os.path.join(pdf_directory, "Irrigation-Optimization.pdf"))
+    thread = threading.Thread(target=send_email, args=(email, pdf_directory))
+    thread.start()
 
     return pdf_directory
+
+def send_email(recipient, pdf_directory):
+    try:
+        me = "ctrl.alt.elite.taps@gmail.com"
+
+        # Create message container
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = "Irrigation Optimization Plan"
+        msg['From'] = me
+        msg['To'] = recipient
+
+        # Create the body of the message (HTML Version)
+        html = """\
+        <html>
+        <head></head>
+        <body>
+            <p>Hi!<br><br>
+            Please see the irrigation optimization report below!<br><br>
+            Best,<br>
+            Crtl + Alt + Elite Team
+            <br>
+            </p>
+        </body>
+        </html>
+        """
+
+        part1 = MIMEText(html, 'html')
+
+        # Attach parts into message container.
+        msg.attach(part1)   
+
+        #Attach the PDF to the email
+        with open(pdf_directory, "rb") as attachment:
+            part = MIMEApplication(attachment.read(), Name=os.path.basename(pdf_directory))
+            part['Content-Disposition'] = f'attachment; filename="{os.path.basename(pdf_directory)}"'
+            msg.attach(part)
+
+        # Send the message via local SMTP server.
+        mail = smtplib.SMTP('smtp.gmail.com', 587)
+
+        mail.ehlo()
+
+        mail.starttls()
+
+        mail.login('ctrl.alt.elite.taps@gmail.com', 'acik plrm mnzf lybx')
+        mail.sendmail(me, recipient, msg.as_string())
+        mail.quit()
+    except Exception as e:
+        print("ERROR: ", str(e))
