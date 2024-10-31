d0fd9bca129d1bff69eb37c999fafc1f36f14f3c
kroy2022 <kevinroy2015@gmail.com>
2024-10-30 22:47:25 -0500

    css styling

--- frontend/src/ButtonPage.jsx
+++ frontend/src/ButtonPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useLocation } from 'react-router-dom';
 import './ButtonPage.css';
+import {Home} from 'lucide-react';
 
 const ButtonPage = () => {
   const { id } = useParams();  // Get the button ID from the URL
@@ -22,6 +23,18 @@ const ButtonPage = () => {
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const [displayedText, setDisplayedText] = useState('');
   const [isDeleting, setIsDeleting] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(null);
+  const dates = [
+    "2024-04-25", "2024-05-23", "2024-06-11", "2024-06-18", "2024-06-25", "2024-07-02", 
+    "2024-07-09", "2024-07-10", "2024-07-16", "2024-07-17", "2024-07-23", "2024-07-23", 
+    "2024-07-30", "2024-08-06", "2024-08-07", "2024-08-13", "2024-08-20", "2024-08-27", 
+    "2024-09-03", "2024-09-10"
+  ];
+  
+  const dateDict = dates.reduce((dict, date, index) => {
+    dict[date] = index;
+    return dict;
+  }, {});
   
   useEffect(() => {
     if (isLoading) {
@@ -53,12 +66,10 @@ const ButtonPage = () => {
   useEffect(() => {
     if (!weatherData) {
       setWeatherData({});
-      console.log("email", email);
-      console.log("famr_id", id);
       const fd = new FormData();
       fd.append("farm_id", id);
       fd.append("email", email)
-      axios.post("http://127.0.0.1:5000/get/plot/info", fd, {
+      axios.post("http://127.0.0.1:5000/future/plot/info", fd, {
         headers: {
           'Content-Type': 'multipart/form-data',
         }
@@ -104,6 +115,13 @@ const ButtonPage = () => {
       return Math.round(gallons * 100) / 100;
   }
 
+  const handleSelectChange = (event) => {
+    const index = event.target.value;
+    setSelectedIndex(index);
+    console.log("Selected Date:", dates[index]); 
+    console.log("Selected Index:", index); 
+  };
+
   if (isLoading) {
     return (
       <div className='loading-container'>
@@ -117,32 +135,67 @@ const ButtonPage = () => {
 
   return (
     <div className='optimize-container'>
-      <div className='data-container'>
-        <div className='header'>
-          <h1 className='h'>Week: {selectedWeek}</h1>
+      <div className='toggle-container'>
+        <div className='items'>
+          <div className='change-date-div'>
+            <label htmlFor="dateDropdown" className='select-date-h'>Select Date:</label>
+            <div className="select-container">
+              <select
+                id="dateDropdown"
+                value={selectedIndex}
+                onChange={handleSelectChange}
+                className="modern-select"
+              >
+                {dates.map((date, index) => (
+                  <option key={index} value={index}>
+                    {date}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className='optimize-btn'>
+            <button className="button">
+              ✨ Optimize
+            </button>
+          </div>
         </div>
-        <div className='pop-divs'>
-          <div className='popup'>
-            <h3 className='pop-h3'>Optimized Irrigation</h3>
-            <h1 className='pop-h'>{weatherData.optimal_irrigation}mm</h1>
-            <h1 className='pop-h'>{calculateOptimalIrrigationPercentage()}% optimization</h1>
+      </div>
+      <div className='right-side'>
+        <div className='navbar'>
+          <div className="navigate">
+            <Home size={30} className="icon" strokeWidth={2} />
+            <h1 className='nav'>Dashboard</h1>
           </div>
-          <div className='popup'>
-            <h3 className='pop-h3'>Cost</h3>
-            <h1 className='pop-h'>${Math.round(weatherData.money_info.plot_cost_optimized * 100) / 100}/plot</h1>
-            <h1 className='pop-h'>${calculateOptimizedPercentSavings()} saved</h1>
+        </div>
+        <div className='data-container'>
+          <div className='header'>
+            <h1 className='h'>Week: {selectedWeek}</h1>
           </div>
-          <div className='popup'>
-            <h3 className='pop-h3'>Energy</h3>
-            <h1 className='pop-h'>{weatherData.energy_info.plot_optimized_energy} kw/h saved</h1>
-            <h1 className='pop-h'>{calculateGallonsSaved()} gallons saved</h1>
-          </div>
-          <div className='popup'>
-            <h3 className='pop-h3'>Weather</h3>
-            <h3 className='pop-h'>Avg Temp: {weatherData.avg_temp} f</h3>
-            <h3 className='pop-h'>Max Temp: {weatherData.max_temp} f</h3>
-            <h3 className='pop-h'>Min Temp: {weatherData.min_temp} f</h3>
-            <h3 className='pop-h'>Avg Humidity: {weatherData.avg_humidity} f</h3>
+          <div className='data-div'>
+            <div className='pop-divs'>
+              <div className='popup'>
+                <h3 className='pop-h3'>Optimized Irrigation</h3>
+                <h1 className='pop-h'>{weatherData.optimal_irrigation} in</h1>
+                <h1 className='pop-h'>{calculateOptimalIrrigationPercentage()}% optimization</h1>
+              </div>
+              <div className='popup'>
+                <h3 className='pop-h3'>Cost</h3>
+                <h1 className='pop-h'>${Math.round(weatherData.money_info.plot_cost_optimized * 100) / 100}/plot</h1>
+                <h1 className='pop-h'>${calculateOptimizedPercentSavings()} saved</h1>
+              </div>
+              <div className='popup'>
+                <h3 className='pop-h3'>Energy</h3>
+                <h1 className='pop-h'>{weatherData.energy_info.plot_optimized_energy} kw/h saved</h1>
+                <h1 className='pop-h'>{calculateGallonsSaved()} gallons saved</h1>
+              </div>
+              <div className='popup'>
+                <h3 className='pop-h3'>Weather</h3>
+                <h3 className='pop-h'>Avg Temp: {weatherData.avg_temp} f</h3>
+                <h3 className='pop-h'>Max Temp: {weatherData.max_temp} f</h3>
+                <h3 className='pop-h'>Min Temp: {weatherData.min_temp} f</h3>
+              </div>
+            </div>
           </div>
         </div>
       </div>
